de98a313c855d875f8e362b34982085314be2010
alexm <alex-m@outlook.com>
2023-08-10 17:06:39 -0700

    Create basic structure of Routes component

--- src/Routes.js
+++ src/Routes.js
@@ -1,1 +1,24 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import DogList from "./DogList";
+import DogDetails from "./DogDetails";
+
+
+function Routes () {
+    return (
+        <Switch>
+            <Route exact path="/dogs">
+                <DogList />
+            </Route>
+
+            <Route path="/dogs/:name">
+                <DogDetails />
+            </Route>
+
+            <Redirect to="/dogs"/>
+        </Switch>
+    );
+}
+
+
+export default Routes;
