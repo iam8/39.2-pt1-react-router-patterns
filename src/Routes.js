7728c4379b78d31bc1f37dcfd327adf777aa3afe
alexm <alex-m@outlook.com>
2023-08-10 17:57:21 -0700

    Add prop of all dog info and pass to Dog List

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+
 import DogList from "./DogList";
 import DogDetails from "./DogDetails";
 
 
-function Routes () {
+function Routes ({allDogInfo}) {
+
     return (
         <Switch>
             <Route exact path="/dogs">
-                <DogList />
+                <DogList allDogInfo={allDogInfo}/>
             </Route>
 
             <Route path="/dogs/:name">
