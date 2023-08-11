8333052e97de795b5fdea20c81c79d0933ee9829
alexm <alex-m@outlook.com>
2023-08-10 17:56:18 -0700

    Get name from route params and display on page

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 
-function DogDetails () {
+function DogDetails ({allDogInfo}) {
+    const {name} = useParams();
+
     return (
         <div className="DogDetails">
+            <h1>{name.toUpperCase()}</h1>
+
             Details on a dog go here
+            {allDogInfo}
         </div>
     )
 }
