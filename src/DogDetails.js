0f49b539d883b625df71344a2df029d2c43439b0
alexm <alex-m@outlook.com>
2023-08-10 19:04:45 -0700

    Display dog details in an organized manner

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,16 +1,44 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+import "./DogDetails.css";
+
 
 function DogDetails ({allDogInfo}) {
     const {name} = useParams();
 
+    // Get the matching dog
+    const dog = allDogInfo.find(dog => dog.name.toLowerCase() === name);
+
     return (
         <div className="DogDetails">
-            <h1>{name.toUpperCase()}</h1>
+            <h1 className="DogDetails-heading">{name.toUpperCase()}</h1>
 
-            Details on a dog go here
-            {allDogInfo}
+            <div className="DogDetails-img-div">
+                <img
+                    className="DogDetails-img"
+                    src={dog.src}
+                    alt={`${dog.name} the dog`}
+                />
+            </div>
+
+            <div className="DogDetails-info-text">
+                <b>Age:</b> {dog.age}
+            </div>
+
+            <div className="DogDetails-info-text">
+                <b>Facts:</b>
+                <ul>
+                    {
+                        dog.facts.map((fact, index) => {
+                            return (
+                                <li key={index}>{fact}</li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+
         </div>
     )
 }
