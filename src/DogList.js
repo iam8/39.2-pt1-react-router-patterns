edab34e65fef8875e26c4886ef32d424f3246cca
alexm <alex-m@outlook.com>
2023-08-10 18:18:24 -0700

    Render a list of dog names (with links) and their image

--- src/DogList.js
+++ src/DogList.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./DogList.css";
 
 
 function DogList ({allDogInfo}) {
     return (
         <div className="DogList">
-            <h1>AVAILABLE DOGS</h1>
+            <h1 className="DogList-heading">AVAILABLE DOGS</h1>
 
-            A list of dogs goes here
+            <div className="DogList-list">
+                <ul>
+                    {allDogInfo.map(dog => {
+                        return (
+                            <li key={dog.name}>
+                                <Link to={`/dogs/${dog.name.toLowerCase()}`}>{dog.name}</Link>
+
+                                <div>
+                                    <img className="DogList-img" src={dog.src} alt={dog.name}/>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+
         </div>
     )
 }
